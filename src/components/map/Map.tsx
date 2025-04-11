6269d81db09b7bd1844058da7e0d08717ca34fe6
Miro Waltisberg <miro@swissfunded.com>
2025-04-11 21:57:22 +0200

    feat: add more countries to global user distribution map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -33,16 +33,43 @@ const Map: React.FC = () => {
   // Generate random signup data
   useEffect(() => {
     const countryData: CountryData[] = [
+      // Americas
       { code: 'US', name: 'United States', region: 'Americas', coordinates: [-95.7129, 37.0902] as [number, number] },
+      { code: 'CA', name: 'Canada', region: 'Americas', coordinates: [-106.3468, 56.1304] as [number, number] },
+      { code: 'BR', name: 'Brazil', region: 'Americas', coordinates: [-51.9253, -14.2350] as [number, number] },
+      { code: 'MX', name: 'Mexico', region: 'Americas', coordinates: [-102.5528, 23.6345] as [number, number] },
+      { code: 'AR', name: 'Argentina', region: 'Americas', coordinates: [-63.6167, -38.4161] as [number, number] },
+      
+      // Europe
       { code: 'GB', name: 'United Kingdom', region: 'Europe', coordinates: [-0.1278, 51.5074] as [number, number] },
       { code: 'DE', name: 'Germany', region: 'Europe', coordinates: [10.4515, 51.1657] as [number, number] },
-      { code: 'JP', name: 'Japan', region: 'Asia', coordinates: [138.2529, 36.2048] as [number, number] },
-      { code: 'AU', name: 'Australia', region: 'Oceania', coordinates: [133.7751, -25.2744] as [number, number] },
-      { code: 'CA', name: 'Canada', region: 'Americas', coordinates: [-106.3468, 56.1304] as [number, number] },
-      { code: 'CH', name: 'Switzerland', region: 'Europe', coordinates: [8.2275, 46.8182] as [number, number] },
-      { code: 'SG', name: 'Singapore', region: 'Asia', coordinates: [103.8198, 1.3521] as [number, number] },
       { code: 'FR', name: 'France', region: 'Europe', coordinates: [2.2137, 46.2276] as [number, number] },
-      { code: 'ES', name: 'Spain', region: 'Europe', coordinates: [-3.7492, 40.4637] as [number, number] }
+      { code: 'CH', name: 'Switzerland', region: 'Europe', coordinates: [8.2275, 46.8182] as [number, number] },
+      { code: 'ES', name: 'Spain', region: 'Europe', coordinates: [-3.7492, 40.4637] as [number, number] },
+      { code: 'IT', name: 'Italy', region: 'Europe', coordinates: [12.5674, 41.8719] as [number, number] },
+      { code: 'NL', name: 'Netherlands', region: 'Europe', coordinates: [5.2913, 52.1326] as [number, number] },
+      { code: 'SE', name: 'Sweden', region: 'Europe', coordinates: [18.6435, 60.1282] as [number, number] },
+      { code: 'NO', name: 'Norway', region: 'Europe', coordinates: [8.4689, 60.4720] as [number, number] },
+      
+      // Asia
+      { code: 'JP', name: 'Japan', region: 'Asia', coordinates: [138.2529, 36.2048] as [number, number] },
+      { code: 'SG', name: 'Singapore', region: 'Asia', coordinates: [103.8198, 1.3521] as [number, number] },
+      { code: 'KR', name: 'South Korea', region: 'Asia', coordinates: [127.7669, 35.9078] as [number, number] },
+      { code: 'CN', name: 'China', region: 'Asia', coordinates: [104.1954, 35.8617] as [number, number] },
+      { code: 'IN', name: 'India', region: 'Asia', coordinates: [78.9629, 20.5937] as [number, number] },
+      { code: 'AE', name: 'UAE', region: 'Asia', coordinates: [53.8478, 23.4241] as [number, number] },
+      { code: 'HK', name: 'Hong Kong', region: 'Asia', coordinates: [114.1694, 22.3193] as [number, number] },
+      
+      // Oceania
+      { code: 'AU', name: 'Australia', region: 'Oceania', coordinates: [133.7751, -25.2744] as [number, number] },
+      { code: 'NZ', name: 'New Zealand', region: 'Oceania', coordinates: [174.8860, -40.9006] as [number, number] },
+      
+      // Africa
+      { code: 'ZA', name: 'South Africa', region: 'Africa', coordinates: [22.9375, -30.5595] as [number, number] },
+      { code: 'EG', name: 'Egypt', region: 'Africa', coordinates: [30.8025, 26.8206] as [number, number] },
+      { code: 'NG', name: 'Nigeria', region: 'Africa', coordinates: [8.6753, 9.0820] as [number, number] },
+      { code: 'KE', name: 'Kenya', region: 'Africa', coordinates: [37.9062, -0.0236] as [number, number] },
+      { code: 'MA', name: 'Morocco', region: 'Africa', coordinates: [-7.0926, 31.7917] as [number, number] }
     ].map(country => ({
       ...country,
       users: Math.floor(Math.random() * 100) + 20 // Random number between 20-120
